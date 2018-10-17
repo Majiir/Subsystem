04493308050b060fbac17dca731255427f99e3ad
Majiir Paktu <majiir@nabaal.net>
2018-10-17 18:58:51 -0400

    Create methods for applying attribute patches to entity type collections

--- Subsystem/AttributeLoader.cs
+++ Subsystem/AttributeLoader.cs
@@ -16,5 +16,36 @@ namespace Subsystem
                 Resource2Cost = 55,
             });
         }
+
+        public static void ApplyAttributesPatch(EntityTypeCollection entityTypeCollection, AttributesPatch attributesPatch)
+        {
+            foreach (var kvp in attributesPatch.Entities)
+            {
+                var entityTypeName = kvp.Key;
+                var entityTypePatch = kvp.Value;
+
+                var entityType = entityTypeCollection.GetEntityType(entityTypeName);
+
+                var unitAttributes = entityType.Get<UnitAttributes>();
+                var unitAttributesWrapper = new UnitAttributesWrapper(unitAttributes);
+
+                ApplyUnitAttributesPatch(entityTypePatch.UnitAttributes, unitAttributesWrapper);
+
+                entityType.Replace(unitAttributes, unitAttributesWrapper);
+            }
+        }
+
+        public static void ApplyUnitAttributesPatch(UnitAttributesPatch unitAttributesPatch, UnitAttributesWrapper unitAttributesWrapper)
+        {
+            if (unitAttributesPatch.MaxHealth.HasValue)
+            {
+                unitAttributesWrapper.MaxHealth = unitAttributesPatch.MaxHealth.Value;
+            }
+
+            if (unitAttributesPatch.Armour.HasValue)
+            {
+                unitAttributesWrapper.Armour = unitAttributesPatch.Armour.Value;
+            }
+        }
     }
 }
