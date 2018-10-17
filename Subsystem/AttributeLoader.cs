1b74fda1f73f13ef8fa12183a92f05cef6d1fc7d
Majiir Paktu <majiir@nabaal.net>
2018-10-17 18:15:03 -0400

    Create AttributeLoader with a dummy LoadAttributes() hook

---
+++ Subsystem/AttributeLoader.cs
@@ -0,0 +1,20 @@
+﻿using BBI.Core.Data;
+using BBI.Game.Data;
+
+namespace Subsystem
+{
+    public class AttributeLoader
+    {
+        public static void LoadAttributes(EntityTypeCollection entityTypeCollection)
+        {
+            var entityType = entityTypeCollection.GetEntityType("G_Baserunner_MP");
+            var unitAttributes = entityType.Get<UnitAttributes>();
+            entityType.Replace(unitAttributes, new UnitAttributesWrapper(unitAttributes)
+            {
+                MaxHealth = 20000,
+                Armour = 50,
+                Resource2Cost = 55,
+            });
+        }
+    }
+}
