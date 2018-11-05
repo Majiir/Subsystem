299d07bdebc32f8a05c274d3eb8b394c2705375d
AGameAnx <agameanx@gmail.com>
2018-11-05 18:48:08 +0200

    Fix bad rebase (calling func)

--- Subsystem/AttributeLoader.cs
+++ Subsystem/AttributeLoader.cs
@@ -505,62 +505,12 @@ namespace Subsystem
             applyPropertyPatch(weaponAttributesPatch.StatusEffectsTargetAlignment, () => weaponAttributesWrapper.StatusEffectsTargetAlignment);
             applyPropertyPatch(weaponAttributesPatch.StatusEffectsExcludeTargetType, () => weaponAttributesWrapper.StatusEffectsExcludeTargetType);
             applyPropertyPatch(weaponAttributesPatch.ActiveStatusEffectsIndex, () => weaponAttributesWrapper.ActiveStatusEffectsIndex);
-            
+
             applyEntityTypesToSpawnOnImpact(weaponAttributesPatch, weaponAttributesWrapper);
 
             if (weaponAttributesPatch.OutputDPS == true)
             {
-                using (logger.BeginScope($"DPS info:"))
-                {
-                    using (logger.BeginScope($"Weapon stats:"))
-                    {
-                        logger.Log($"BaseDamagePerRound {weaponAttributesWrapper.BaseDamagePerRound}");
-                        logger.Log($"RateOfFire {weaponAttributesWrapper.RateOfFire}");
-                        logger.Log($"Burst: {weaponAttributesWrapper.BurstPeriodMinTimeMS} - {weaponAttributesWrapper.BurstPeriodMaxTimeMS}");
-                        logger.Log($"NumberOfBursts {weaponAttributesWrapper.NumberOfBursts}");
-                        logger.Log($"CooldownTimeMS: {weaponAttributesWrapper.CooldownTimeMS}");
-                        logger.Log($"WindUpTimeMS: {weaponAttributesWrapper.WindUpTimeMS}");
-                        logger.Log($"WindDownTimeMS: {weaponAttributesWrapper.WindDownTimeMS}");
-                        logger.Log($"ReloadTimeMS {weaponAttributesWrapper.ReloadTimeMS}");
-                        logger.Log($"DamagePacketsPerShot {weaponAttributesWrapper.DamagePacketsPerShot}");
-                        logger.Log($"AreaOfEffectFalloffType {weaponAttributesWrapper.AreaOfEffectFalloffType}");
-                        logger.Log($"AreaOfEffectRadius {weaponAttributesWrapper.AreaOfEffectRadius}");
-                    }
-
-                    int burstVariance = weaponAttributesWrapper.BurstPeriodMaxTimeMS - weaponAttributesWrapper.BurstPeriodMinTimeMS;
-
-                    int shotDuration = Math.Max(1, 1000 / weaponAttributesWrapper.RateOfFire);
-
-                    int shotsMin = Math.Max(1, weaponAttributesWrapper.BurstPeriodMinTimeMS / shotDuration);
-                    int shotsMax = Math.Max(1, weaponAttributesWrapper.BurstPeriodMaxTimeMS / shotDuration);
-
-                    int lowBracket = Math.Min(weaponAttributesWrapper.BurstPeriodMinTimeMS, shotDuration - 1 - (weaponAttributesWrapper.BurstPeriodMinTimeMS - shotsMin * shotDuration));
-                    int midBracket = Math.Max(0, shotsMax - shotsMin - 1) * shotDuration;
-                    int highBracket = shotsMin < shotsMax ? weaponAttributesWrapper.BurstPeriodMaxTimeMS - shotsMax * shotDuration + 1 : 0;
-
-                    double avgBurst = (weaponAttributesWrapper.BurstPeriodMaxTimeMS + weaponAttributesWrapper.BurstPeriodMinTimeMS) * 0.5;
-
-                    double averageShotsPerBurst = burstVariance == 0 ? shotsMin : (double)(shotsMin * lowBracket + (shotsMax + shotsMin) / 2 * midBracket + shotsMax * highBracket) / burstVariance;
-                    double trueROF = averageShotsPerBurst / avgBurst * 1000;
-
-                    double sequenceDuration = (weaponAttributesWrapper.WindUpTimeMS + (avgBurst + weaponAttributesWrapper.CooldownTimeMS) * (weaponAttributesWrapper.NumberOfBursts - 1) + avgBurst + weaponAttributesWrapper.ReloadTimeMS) / 1000;
-
-                    logger.Log($"averageShotsPerBurst: {averageShotsPerBurst}");
-                    logger.Log($"trueROF: {trueROF}");
-                    logger.Log($"sequenceDuration: {sequenceDuration}");
-
-                    for (double armor = 0; armor <= 18; armor += 6)
-                    {
-                        using (logger.BeginScope($"Armor {armor} DPS:"))
-                        {
-                            double dps = Math.Max(1, Fixed64.UnsafeDoubleValue(weaponAttributesWrapper.BaseDamagePerRound) - armor * weaponAttributesWrapper.DamagePacketsPerShot) * averageShotsPerBurst * weaponAttributesWrapper.NumberOfBursts / sequenceDuration;
-                            foreach (var range in weaponAttributesWrapper.Ranges)
-                            {
-                                logger.Log($"{range.Range,6} ({range.Distance,5:D} /{(int)Fixed64.UnsafeDoubleValue(range.Accuracy),3:D}%): {dps * Fixed64.UnsafeDoubleValue(range.Accuracy) / 100:F4}");
-                            }
-                        }
-                    }
-                }
+                outputWeaponDPS(weaponAttributesWrapper);
             }
         }
 
