88bc297067c2dd343041da371ce1d03e748d0f35
AGameAnx <agameanx@gmail.com>
2018-12-06 17:50:30 +0200

    Status effects WIP

---
+++ Subsystem/Patch/HealthOverTimeAttributesPatch.cs
@@ -0,0 +1,12 @@
+﻿using BBI.Game.Data;
+
+namespace Subsystem.Patch
+{
+    public class HealthOverTimeAttributesPatch
+    {
+        public string ID { get; set; }
+        public int? Amount { get; set; }
+        public int? MSTickDuration { get; set; }
+        public DamageType? DamageType;
+    }
+}
---
+++ Subsystem/Patch/StatusEffectAttributesPatch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using BBI.Game.Data;
+
+namespace Subsystem.Patch
+{
+    public class StatusEffectAttributesPatch : IRemovable
+    {
+        public StatusEffectLifetime Lifetime { get; set; }
+        public double Duration { get; set; }
+        public WeaponFireTrigger WeaponFireTriggerEndEvent { get; set; }
+        public int MaxStacks { get; set; }
+        public StackingBehaviour StackingBehaviour { get; set; }
+
+        public Dictionary<string, AttributeBuffPatch> BuffsToApplyToTarget { get; set; } = new Dictionary<string, AttributeBuffPatch>();
+        public Dictionary<string, AttributeBuffPatch> UnitTypeBuffsToApply { get; set; } = new Dictionary<string, AttributeBuffPatch>();
+
+        public Dictionary<string, ModifierPatch> Modifiers { get; set; } = new Dictionary<string, ModifierPatch>();
+
+        public bool Remove { get; set; }
+    }
+}
